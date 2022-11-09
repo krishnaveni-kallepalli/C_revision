379efc586df610e1c478e1b143df311120e7a858
krish <krishnaveni.k@thundersoft.com>
2022-11-09 17:47:37 +0530

    09/11/2022

--- basics/multiply_without_using_asterisk.c
+++ basics/multiply_without_using_asterisk.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
-int main()
+void mul(int x,int y)
 {
-	int x,y,p=0;
-	scanf("%d %d",&x,&y);
-	
+	int p=0;
 	while(y!=0)
 	{
 		p=p+x;
 		y--;
 	}
 	printf("%d\n",p);
+}
+int main()
+{
+	int x,y;
+	scanf("%d %d",&x,&y);
+	
+	void (*fp)(int,int)=mul;
+	fp(x,y);
 	
 }
